b21ecfdb02c33ce80737582ca50717fa7bfdebff
Hope <JH075759@cerner.net>
2020-01-09 08:29:48 -0600

    last commit from jh075759

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 // Write your JavaScript code here.
-// Remember to pay attention to page loading!
+// Remember to pay attention to page loading!\\
+
+
+
+window.addEventListener("load", function () {
+    // put DOM code here to ensure elements have been loaded
+    function launchConfirm() {
+        let response = window.confirm("Would you like to play a game?");
+        // Code does NOT continue until user responds to confirm window
+        if (response === true) {
+            let answer = window.confirm("Message to user");
+        } else {
+            console.log("Negative");
+        }
+    }
+
+});
